5aec43e1f7cbf62070abbb5dd8b84c5d5f96a288
pshandy <nikonovmd12@mail.ru>
2022-09-13 16:16:35 +0300

    1. Удалить файл Class1.cs 2. Добавить в проект папку src 3. Добавить в проект папку src\Sample 4. Добавить в папку src\Sample класс SampleTests 5. Сделать этот класс открытым (public) 6. Добавить к классу атрибут [TestFixture] и ссылку на пространство имен NUnit.Framework; 7. Добавить в класс SampleTests метод открытый метод без параметров Demo.

---
+++ KTPO4317.Nikonov.UnitTest/src/Sample/SampleTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace KTPO4317.Nikonov.UnitTest.src.Sample
+{
+    [TestFixture]
+    class SampleTests
+    {
+        [Test]
+        public void Demo()
+        {
+            Assert.Pass();
+        }
+
+    }
+
+}
