122dd34cf4a529b4fd5d5536ea80de68c21f1224
pshandy <nikonovmd12@mail.ru>
2022-09-13 16:33:41 +0300

    1. Добавить в проект «.Lib» папку src\LogAn 2. Добавить в созданную класс LogAnalyzer 3. В класс добавить один публичный метод IsValidLogFileName, проверяющий правильность расширения файла: расширение файла “.NMD”.

---
+++ KTPO4317.Nikonov.Lib/src/LogAn/LogAnalyzer.cs
@@ -0,0 +1,15 @@
+﻿
+namespace KTPO4317.Nikonov.Lib.src.LogAn
+{
+    class LogAnalyzer
+    {
+        public bool IsValidLogFileName(string fileName)
+        {
+            if (fileName.EndsWith(".NMD"))
+            {
+                return (false);
+            }
+            return (true);
+        }
+    }
+}
