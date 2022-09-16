69c41ecc855dd20660a1def61da6d1725276e836
pshandy <nikonovmd12@mail.ru>
2022-09-16 22:24:45 +0300

    2.1. Удалены все тесты из LogAnalyzerTests. Добавлен псевдокод в LogAnalyzer.

--- KTPO4317.Nikonov.Lib/src/LogAn/LogAnalyzer.cs
+++ KTPO4317.Nikonov.Lib/src/LogAn/LogAnalyzer.cs
@@ -4,20 +4,12 @@ namespace KTPO4317.Nikonov.Lib.src.LogAn
 {
 
     public class LogAnalyzer
-    {
-
-        public bool WasLastFileNameValid { get; set; }
+    { 
         public bool IsValidLogFileName(string fileName)
         {
-            if (string.IsNullOrEmpty(fileName))
-            {
-                throw new ArgumentException("Имя файла должно быть задано.");
-            }
-            if (!fileName.ToUpper().EndsWith(".NMD"))
-            {
-                return (false);
-            }
-            WasLastFileNameValid = true;
+            //читать конфигурационный файл
+            //вернуть true
+            //если конфигурация поддерживается
             return (true);
         }
     }
--- KTPO4317.Nikonov.UnitTest/src/LogAn/LogAnalyzerTests.cs
+++ KTPO4317.Nikonov.UnitTest/src/LogAn/LogAnalyzerTests.cs
@@ -7,55 +7,8 @@ namespace KTPO4317.Nikonov.UnitTest.src.LogAn
     [TestFixture]
     class LogAnalyzerTests
     {
-        [Test]
-        public void IsValidLogFileName_BadExtension_ReturnFalse()
-        {
-            LogAnalyzer analyzer = new LogAnalyzer();
-            bool result = analyzer.IsValidLogFileName("file.txt");
-            Assert.False(result);
-        }
 
-        [Test]
-        public void IsValidLogFileName_GoodExtensionUppercase_ReturnsTrue()
-        {
-            LogAnalyzer analyzer = new LogAnalyzer();
-            bool result = analyzer.IsValidLogFileName("file.NMD");
-            Assert.True(result);
-        }
-
-        [Test]
-        public void IsValidLogFileName_GoodExtensionLowercase_ReturnsTrue()
-        {
-            LogAnalyzer analyzer = new LogAnalyzer();
-            bool result = analyzer.IsValidLogFileName("file.nmd");
-            Assert.True(result);
-        }
-
-        [TestCase("file.NMD")]
-        [TestCase("file.nmd")]
-        public void IsValidLogFileName_ValidExtension_ReturnsTrue(string file)
-        {
-            LogAnalyzer analyzer = new LogAnalyzer();
-            bool result = analyzer.IsValidLogFileName(file);
-            Assert.True(result);
-        }
-
-        [Test]
-        public void IsValidLogFileName_EmptyFileName_Throws()
-        {
-            LogAnalyzer analyzer = new LogAnalyzer();
-            var ex = Assert.Catch<Exception>(() => analyzer.IsValidLogFileName(""));
-            StringAssert.Contains("Имя файла должно быть задано.", ex.Message);
-        }
-
-        [TestCase("badfile.foo", false)]
-        [TestCase("file.nmd", true)]
-        public void IsValidLogFileName_WhenCalled_ChangesWasLastFileNameValid(string file, bool expected)
-        {
-            LogAnalyzer analyzer = new LogAnalyzer();
-            analyzer.IsValidLogFileName(file);
-            Assert.AreEqual(expected, analyzer.WasLastFileNameValid);
-        }
+           
 
     }
 }
