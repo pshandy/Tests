45427fc28f43a28d99c3592840863ead58df4cc1
pshandy <nikonovmd12@mail.ru>
2022-09-17 20:08:02 +0300

    5.3. Подписка объекта на событие. Добавлен интерфейс IView. Добавлен класс Presenter.

---
+++ KTPO4317.Nikonov.Lib/src/LogAn/IView.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KTPO4317.Nikonov.Lib.src.LogAn
+{
+    public interface IView
+    {
+        void Render(string text);
+    }
+}
---
+++ KTPO4317.Nikonov.Lib/src/LogAn/Presenter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KTPO4317.Nikonov.Lib.src.LogAn
+{
+    public class Presenter
+    {
+
+        private LogAnalyzer LogAnalyzer;
+        private IView View;
+
+        public Presenter(LogAnalyzer LogAnalyzer, IView View)
+        {
+            this.LogAnalyzer = LogAnalyzer;
+            this.View = View;
+            LogAnalyzer.Analyzed += OnLogAnalyzer;
+        }
+
+        private void OnLogAnalyzer()
+        {
+            View.Render("Обработка завершена");
+        }
+
+    }
+}
